8846ac37f5398db5e5b61d801438c562e7dc6dec
hezron99 <hezronmmisagal@gmail.com>
2023-01-31 13:13:47 +0800

    add all

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '167e8753ad8cbd0eec285ab0440ed1026724b344',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '167e8753ad8cbd0eec285ab0440ed1026724b344',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v229',
+            'version' => '229.0.0.0',
+            'reference' => '9f955886f1ed73078482ac44dcd750693426c42c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
